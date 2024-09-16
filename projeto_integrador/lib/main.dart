1fd3415da2a9690818f2d827311f4ee72f72756e
PedroPiveta <ppivetabarrotti208@gmail.com>
2024-09-16 19:05:24 -0300

    creates blank pages

--- projeto_integrador/lib/main.dart
+++ projeto_integrador/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:projeto_integrador/pages/create_account_page.dart';
 import 'package:projeto_integrador/pages/home_page.dart';
 import 'package:projeto_integrador/pages/login_page.dart';
 import 'package:projeto_integrador/themes/light_mode.dart';
+import 'package:projeto_integrador/pages/notifications_page.dart';
+import 'package:projeto_integrador/pages/users_page.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -10,7 +12,9 @@ void main() {
     routes: {
       '/login': (context) => LoginPage(),
       '/createAccount': (context) => CreateAccountPage(),
-      '/home': (context) => HomePage()
+      '/home': (context) => HomePage(),
+      '/users': (context) => UsersPage(),
+      '/notifications': (context) => NotificationsPage(),
     },
     theme: lightMode,
     debugShowCheckedModeBanner: false,
---
+++ projeto_integrador/lib/pages/users_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_integrador/components/custom_app_bar.dart';
+import 'package:projeto_integrador/components/custom_menu_bar.dart';
+
+class UsersPage extends StatefulWidget {
+  const UsersPage({super.key});
+
+  @override
+  State<UsersPage> createState() => _UsersPageState();
+}
+
+class _UsersPageState extends State<UsersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(),
+      bottomNavigationBar: CustomMenuBar(),
+    );
+  }
+}
