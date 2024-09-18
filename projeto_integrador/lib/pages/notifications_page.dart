9f467c0001439ff8bcfabf56a523fa900d592ba9
Lucas Fernandes <lucas.fernandes.df462@gmail.com>
2024-09-17 21:46:58 -0300

    feat: Criação do layout de notificações

--- projeto_integrador/lib/pages/notifications_page.dart
+++ projeto_integrador/lib/pages/notifications_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_integrador/components/custom_app_bar.dart';
+import 'package:projeto_integrador/components/custom_menu_bar.dart';
+import 'package:projeto_integrador/components/custom_notification.dart';
 
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({super.key});
@@ -10,6 +13,28 @@ class NotificationsPage extends StatefulWidget {
 class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 40),
+        child: Column(
+          children: [
+            Text(
+              "Avisos",
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.onSurface,
+                  fontSize: 36,
+                  fontWeight: FontWeight.w600),
+            ),
+            const Divider(
+              thickness: 1,
+              color: Color(0xFFCAC4D0),
+            ),
+            const SizedBox(height: 40,),
+            
+            CustomNotification(title: "Aviso", message: "Vence amanhã"),
+          ],
+        ),
+      ),
+    );
   }
 }
